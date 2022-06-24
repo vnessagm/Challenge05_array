f66656d4d6382f487b1d2dc4f9f251d5ceaecfe8
vnessagm <nessagm124@gmail.com>
2022-06-24 10:09:12 -0300

    array exercise

---
+++ script.js
@@ -0,0 +1,19 @@
+// // Crie um array que receba 5 itens e exiba no console.
+let nomer = ["Carlos" ,"André" ,"Luis", "Marcos", "Julio"];
+console.log(nomer)
+// Utilize um método para adicionar um nome ao inicio do array.
+nomer.unshift("Leandro")
+console.log(nomer)
+// Utilize um método para remover o último nome do array.
+nomer.pop()
+console.log(nomer)
+// Utilize um método para adicionar dois nomes ao fim do array.
+nomer.push("Lucas", "José")
+console.log(nomer)
+// Utilize um método para remover o primeiro nome do array.
+nomer.shift()
+console.log(nomer)
+// Utilize um método para organizar em ordem crescente o seguinte array:
+const numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+numbers.sort()
+console.log(numbers)
